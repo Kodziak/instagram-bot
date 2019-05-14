6ca9c960dc8c37db8f503f60dbffe6b00726a4a8
Kodziak <kodziak1416@gmail.com>
2019-05-14 19:33:34 +0200

    Add dependencies

--- __tests__/Instagram.js
+++ __tests__/Instagram.js
@@ -1,19 +1,29 @@
+const puppeteer = require("puppeteer");
 const LoginUtils = require("../utils/LoginUtils");
 const config = require("../config.js");
 
 describe("Instagram-bot.", () => {
   const page = "https://www.instagram.com/";
   const loginPage = "https://www.instagram.com/accounts/login/";
+  jest.setTimeout(10 * 1000);
 
   beforeAll(async () => {
+    await puppeteer.launch();
     await LoginUtils.openBrowserWithPage(loginPage);
-    jest.setTimeout(10000 * 1000);
+  });
+
+  afterAll(async () => {
+    await page.close();
   });
 
   describe("Let's make some subs!", () => {
     test("Goto login card", async () => {
-      console.log(config.login, config.pass);
       await LoginUtils.login(config.login, config.pass);
+
+      let login = await page.evaluate(() => {
+        return document.querySelector(".gmFkV").textContent;
+      });
+      await expect(login).toBe("xxblackx_xbeautyxx");
     });
   });
 });
