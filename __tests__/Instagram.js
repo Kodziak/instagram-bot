a32c9dc20bce8576e91fa1848394ebb4132c694e
Kodziak <kodziak1416@gmail.com>
2019-05-14 17:36:29 +0200

    Add open browser and improve login fuction.

--- __tests__/Instagram.js
+++ __tests__/Instagram.js
@@ -1,18 +1,18 @@
 const LoginUtils = require("../utils/LoginUtils");
-const config = require("../config");
+const config = require("../config.js");
 
 describe("Instagram-bot.", () => {
   const page = "https://www.instagram.com/";
   const loginPage = "https://www.instagram.com/accounts/login/";
 
   beforeAll(async () => {
-    await page.goto(loginPage, {
-      waitUntil: "load"
-    });
+    await LoginUtils.openBrowserWithPage(loginPage);
+    jest.setTimeout(10 * 1000);
   });
 
   describe("Let's make some subs!", () => {
     test("Goto login card", async () => {
+      console.log(config.login, config.pass);
       await LoginUtils.login(config.login, config.pass);
     });
   });
