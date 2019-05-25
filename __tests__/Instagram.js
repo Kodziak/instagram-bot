233f5cbe67c9040ba05e7d117e52af264f8f2ef1
Kodziak <kodziak1416@gmail.com>
2019-05-25 22:54:15 +0200

    Add going to page, like and comment photo.

--- __tests__/Instagram.js
+++ __tests__/Instagram.js
@@ -30,21 +30,16 @@ describe("Instagram-bot.", () => {
         waitUntil: "load"
       });
 
-      const pageTarget = this.page.target(); //save this to know that this was the opener
-      const newTarget = await this.browser.waitForTarget(
-        target => target.opener() === pageTarget
-      ); //check that you opened this page, rather than just checking the url
-      const newPage = await newTarget.page(); //get the page object
-      // await newPage.once("load",()=>{}); //this doesn't work; wait till page is loaded
-      await newPage.waitForSelector(".v1Nh3"); //wait for page to be loaded
+      await page.waitForSelector(".v1Nh3");
 
-      let images = await newPage.$$(".v1Nh3");
-      images.forEach(async image => {
-        await newPage.click(image);
-        await newPage.waitForSelector('[aria-label="Like"]');
-        await newPage.click('[aria-label="Like"]');
-        await newPage.type(".Ypffh", "Excellent picture!");
-      });
+      await page.click(".v1Nh3");
+      await page.waitFor(2000);
+      await page.waitForSelector('[aria-label="Like"]');
+      await page.click('[aria-label="Like"]');
+      await page.waitFor(2000);
+
+      await page.type(".Ypffh", "Excellent picture!");
+      await page.waitFor(2000);
     });
   });
 });
