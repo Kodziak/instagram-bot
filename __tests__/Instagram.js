c1daf4fb0e6bbc2b0a593143840827e550ba2fe5
Kodziak <kodziak1416@gmail.com>
2019-05-25 23:32:17 +0200

    Add random time generator.

--- __tests__/Instagram.js
+++ __tests__/Instagram.js
@@ -1,10 +1,12 @@
 const puppeteer = require("puppeteer");
 const LoginUtils = require("../utils/LoginUtils");
 const config = require("../config.js");
+const HEART_FILLED_SELECTOR = "glyphsSpriteHeart__filled__24__red_5 u-__7";
+const TAG = "car";
+const RANDOM_TIME = 2250 + Math.floor(Math.random() * 250);
 
 describe("Instagram-bot.", () => {
   const loginPage = "https://www.instagram.com/accounts/login/";
-  const tag = "car";
 
   beforeAll(async () => {
     await puppeteer.launch();
@@ -26,21 +28,44 @@ describe("Instagram-bot.", () => {
     });
 
     test("Goto tag", async () => {
-      await page.goto(`https://www.instagram.com/explore/tags/${tag}/`, {
+      await page.goto(`https://www.instagram.com/explore/tags/${TAG}/`, {
         waitUntil: "load"
       });
 
+      //Click on photo
       await page.waitForSelector(".v1Nh3");
+      let images = await page.$$(".v1Nh3");
+      // console.log(images, images.length);
 
       await page.click(".v1Nh3");
-      await page.waitFor(2000);
-      await page.waitForSelector('[aria-label="Like"]');
-      await page.click('[aria-label="Like"]');
-      await page.waitFor(2000);
+      await page.waitFor(RANDOM_TIME);
 
-      await page.type(".Ypffh", "Excellent picture!");
-      await page.waitFor(2000);
-      await page.click('[type="submit"]');
+      //Click on heart
+      let heartSelector = await page.evaluate(() => {
+        return document.querySelector(".dCJp8").firstElementChild.className;
+      });
+
+      if (heartSelector !== HEART_FILLED_SELECTOR) {
+        await page.waitForSelector('[aria-label="Like"]');
+        await page.click('[aria-label="Like"]');
+        await page.waitFor(RANDOM_TIME);
+      }
+
+      let comments = await page.$$(".TlrDj");
+      comments = Array.from(comments);
+      comments.forEach(comment => {
+        console.log(comment.textContent);
+      });
+      // comments.forEach(comment => {
+      //   console.log(comment.textContent);
+      // });
+
+      // await page.type(".Ypffh", "Excellent picture!");
+      // await page.waitFor(2000);
+      // await page.click('[type="submit"]');
+      // await page.waitFor(2000);
+
+      await page.click(".ckWGn");
     });
   });
 });
